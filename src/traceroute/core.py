134f31eb86947d3f1c9b72f83267c421017f23db
Ib Helmer Nielsen <ihn@ucn.dk>
2022-02-14 20:35:21 +0100

    Initial commit

---
+++ src/traceroute/core.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2015 Marin Atanasov Nikolov <dnaeon@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer
+#    in this position and unchanged.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+Core module
+
+"""
+
+import socket
+import random
+import struct
+import time
+
+__all__ = ['Tracer']
+
+
+class Tracer(object):
+    def __init__(self, dst, hops=30):
+        """
+        Initializes a new tracer object
+        Args:
+            dst  (str): Destination host to probe
+            hops (int): Max number of hops to probe
+        """
+        self.dst = dst
+        self.hops = hops
+        self.ttl = 1
+
+        # Pick up a random port in the range 33434-33534
+        self.port = random.choice(range(33434, 33535))
+
+    def run(self):
+        """
+        Run the tracer
+
+        Raises:
+            IOError
+
+        """
+        try:
+            dst_ip = socket.gethostbyname(self.dst)
+        except socket.error as e:
+            raise IOError('Unable to resolve {}: {}'.format(self.dst, e))
+
+        text = 'traceroute to {} ({}), {} hops max'.format(
+            self.dst,
+            dst_ip,
+            self.hops
+        )
+
+        print(text)
+
+        while True:
+            startTimer = time.time()
+            receiver = self.create_receiver()
+            sender = self.create_sender()
+            sender.sendto(b'', (self.dst, self.port))
+
+            addr = None
+            try:
+                data, addr = receiver.recvfrom(1024)
+                entTimer = time.time()
+            except socket.error:
+                pass
+                # raise IOError('Socket error: {}'.format(e))
+            finally:
+                receiver.close()
+                sender.close()
+
+            if addr:
+                timeCost = round((entTimer - startTimer) * 1000, 2)
+                print('{:<4} {} {} ms'.format(self.ttl, addr[0], timeCost))
+                if addr[0] == dst_ip:
+                    break
+            else:
+                print('{:<4} *'.format(self.ttl))
+
+            self.ttl += 1
+
+            if self.ttl > self.hops:
+                break
+
+    def create_receiver(self):
+        """
+        Creates a receiver socket
+
+        Returns:
+            A socket instance
+
+        Raises:
+            IOError
+
+        """
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_RAW,
+            proto=socket.IPPROTO_ICMP
+        )
+
+        timeout = struct.pack("ll", 5, 0)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeout)
+
+        try:
+            s.bind(('', self.port))
+        except socket.error as e:
+            raise IOError('Unable to bind receiver socket: {}'.format(e))
+
+        return s
+
+    def create_sender(self):
+        """
+        Creates a sender socket
+
+        Returns:
+            A socket instance
+
+        """
+        s = socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_DGRAM,
+            proto=socket.IPPROTO_UDP
+        )
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_TTL , self.ttl)
+        return s
